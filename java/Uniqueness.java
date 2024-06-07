e022ce82f3821475676a4e9388b43eabbbd16cc8
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-07 15:59:26 +0000

    Add exercise 2 K-Z

--- java/Uniqueness.java
+++ java/Uniqueness.java
@@ -19,4 +19,36 @@ class Uniqueness {
         return false;
     return true;
   }
+
+  public static double measureExecutionTime(IntArrayFunction function, int inputSize) {
+    int[] S = new int[inputSize];
+    for (int i = 0; i < inputSize; i++)
+      S[i] = i;
+    long startTime = System.nanoTime();
+    function.apply(S);
+    long endTime = System.nanoTime();
+    return (endTime - startTime) / 1e9;
+  }
+
+  public static int findMaxInputSizeWithinTimeLimit(IntArrayFunction function, double timeLimit) {
+    int n = 1;
+    while (true) {
+      double executionTime = measureExecutionTime(function, n);
+      if (executionTime > timeLimit)
+        return n / 2;
+      n *= 2;
+    }
+  }
+
+  interface IntArrayFunction {
+    boolean apply(int[] array);
+  }
+
+  public static void main(String[] args) {
+    int maxNUnique1 = findMaxInputSizeWithinTimeLimit(Uniqueness::unique1, 0.1);
+    System.out.println("For the brute-force algorithm, the largest value of n within one minute: " + maxNUnique1);
+
+    int maxNUnique2 = findMaxInputSizeWithinTimeLimit(Uniqueness::unique2, 0.1);
+    System.out.println("For the sorted algorithm, the largest value of n within one minute: " + maxNUnique2);
+  }
 }
