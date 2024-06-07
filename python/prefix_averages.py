fe89cd739b3fb62581be324577b574a6116a0a1a
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-07 15:13:44 +0000

    Add exercise 2 A-J

--- python/prefix_averages.py
+++ python/prefix_averages.py
@@ -1,3 +1,8 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def prefix_average1(S):
     n = len(S)
     A = [0] * n
@@ -15,3 +20,33 @@ def prefix_average2(S):
     for j in range(n):
         A[j] = sum(S[0 : j + 1]) / (j + 1)
     return A
+
+
+input_sizes = [100, 200, 400, 800, 1600, 3200, 6400]
+
+
+times1 = []
+times2 = []
+
+for size in input_sizes:
+    S = np.random.rand(size).tolist()
+
+    start_time = time.time()
+    prefix_average1(S)
+    end_time = time.time()
+    times1.append(end_time - start_time)
+
+    start_time = time.time()
+    prefix_average2(S)
+    end_time = time.time()
+    times2.append(end_time - start_time)
+
+plt.figure(figsize=(10, 6))
+plt.plot(input_sizes, times1, label="prefix_average1", marker="o")
+plt.plot(input_sizes, times2, label="prefix_average2", marker="o")
+plt.xlabel("Input Size (n)")
+plt.ylabel("Execution Time (seconds)")
+plt.title("Execution Time vs Input Size for Prefix Average Algorithms")
+plt.legend()
+plt.grid(True)
+plt.savefig("execution_time_plot.png")
