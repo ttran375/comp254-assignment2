e507a0068ccb694cb31d7cfae6162be0e77331a9
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 16:16:04 +0000

    Update test_find_non_sum_integer.py

--- python/exercise3/find_non_sum_integer.py
+++ python/exercise3/find_non_sum_integer.py
@@ -2,18 +2,18 @@ def find_non_sum_integer(A):
     if len(A) < 2:
         return None
     max_element = max(A)
-    print(2 * max_element + 1)
+    return 2 * max_element + 1
 
 
 def main():
     list1 = []
-    find_non_sum_integer(list1)
+    print(find_non_sum_integer(list1))
 
     list2 = [0]
-    find_non_sum_integer(list2)
+    print(find_non_sum_integer(list2))
 
     list3 = [0, 3, 1, 4, 12, 5, 2]
-    find_non_sum_integer(list3)
+    print(find_non_sum_integer(list3))
 
 
 if __name__ == "__main__":
