a66cbfed43cca3255967fc8277e524f84ce0b18f
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 16:00:20 +0000

    Update test_find_missing_number.py

--- python/exercise3/find_missing_number.py
+++ python/exercise3/find_missing_number.py
@@ -2,18 +2,18 @@ def find_missing_number(A):
     n = len(A) + 1
     total_sum = n * (n - 1) // 2
     array_sum = sum(A)
-    print(total_sum - array_sum)
+    return total_sum - array_sum
 
 
 def main():
     list1 = []
-    find_missing_number(list1)
+    print(find_missing_number(list1))
 
     list2 = [0]
-    find_missing_number(list2)
+    print(find_missing_number(list2))
 
     list3 = [0, 1, 2, 4, 5, 6]
-    find_missing_number(list3)
+    print(find_missing_number(list3))
 
 
 if __name__ == "__main__":
