1f4beae318350fa8ee06f10ec18bd218eff2c96a
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-28 13:46:24 +0000

    Fix linters

--- src/exercise2/prefix_averages.py
+++ src/exercise2/prefix_averages.py
@@ -19,7 +19,7 @@ def prefix_average2(S):
     n = len(S)
     A = [0] * n
     for j in range(n):
-        A[j] = sum(S[0: j + 1]) / (j + 1)
+        A[j] = sum(S[0 : j + 1]) / (j + 1)
     return A
 
 
