97e4a5e429d6bf5ef31ad34e3925131c334fe04c
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-28 13:19:54 +0000

    chore: Update find_missing_number function to improve readability

--- src/exercise3/find_missing_number.py
+++ src/exercise3/find_missing_number.py
@@ -1,20 +1,6 @@
-def find_missing_number(A):
-    n = len(A) + 1
-    total_sum = n * (n - 1) // 2
-    array_sum = sum(A)
-    return total_sum - array_sum
+def find_missing_number(arr):
+    # Calculate the expected number of elements in the array
+    n = len(arr) + 1
 
-
-def main():
-    list1 = []
-    print(find_missing_number(list1))
-
-    list2 = [0]
-    print(find_missing_number(list2))
-
-    list3 = [0, 1, 2, 4, 5, 6]
-    print(find_missing_number(list3))
-
-
-if __name__ == "__main__":
-    main()
+    # Find the missing number
+    return (n * (n - 1) // 2) - sum(arr)
