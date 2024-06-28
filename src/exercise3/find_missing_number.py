eae0dde0a26e27cc0191c19b0ab99b6ea9c80e28
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-28 13:30:49 +0000

    chore: Update find_missing_number function documentation

--- src/exercise3/find_missing_number.py
+++ src/exercise3/find_missing_number.py
@@ -1,4 +1,7 @@
 def find_missing_number(arr):
+    """
+    Finds the missing number in an array containing n-1 unique integers in the range [0, n-1].
+    """
     # Calculate the expected number of elements in the array
     n = len(arr) + 1
 
