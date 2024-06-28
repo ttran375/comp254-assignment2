44a59cef32a7a3850c2c7cec48ec136383e5b8d6
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-28 13:31:05 +0000

    Update find_non_sum_integer documentation

--- src/exercise3/find_non_sum_integer.py
+++ src/exercise3/find_non_sum_integer.py
@@ -1,8 +1,11 @@
 def find_non_sum_integer(A):
+    """
+    Finds a non-sum integer from the given list A.
+    """
     return (
         # If there are at least two elements, calculate the non-sum integer
-        2 * max(A) + 1 if len(A) >= 2
-        
+        2 * max(A) + 1
+        if len(A) >= 2
         # Otherwise, return None
         else None
     )
